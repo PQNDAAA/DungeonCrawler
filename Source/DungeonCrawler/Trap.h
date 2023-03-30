b4c959a235b6fd8647dce47e9df75dee3bf57980
PQNDAAA <gayvallet.dylan@gmail.com>
2023-03-30 02:13:20 +0200

    Initial commit

---
+++ Source/DungeonCrawler/Trap.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/Actor.h"
+#include "Trap.generated.h"
+
+UCLASS()
+class DUNGEONCRAWLER_API ATrap : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATrap();
+
+	UPROPERTY(VisibleAnywhere, Category=Stats)
+	float damage = 10.0f;
+
+	UPROPERTY(VisibleAnywhere, Category="Sphere")
+	USphereComponent* SphereCollider;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent *OverlappedComp,
+		class AActor* OtherActor,
+		class UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult);
+
+};
